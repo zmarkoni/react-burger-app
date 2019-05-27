1ffed0ba95cbf1d3d9836f30e0c4792e747cfc66
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-05-27 23:06:31 +0200

    create layout and burgerBuilder

---
+++ burger-basic/src/components/Burger/BurgerIngridient/BurgerIngridient.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import Aux from '../../hoc/Aux';
+//import classes from './Layout.module.css'
+
+const burgerIngridient = (props) => {
+    let ingredient = null;
+
+
+
+};
+
+export default burgerIngridient;
