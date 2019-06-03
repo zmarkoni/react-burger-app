5823613b3c0c8bbb17c9ecbcef4c1b56bd1c3042
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-06-03 10:54:36 +0200

    finish sideDrawer

--- burger-basic/src/components/Logo/Logo.js
+++ burger-basic/src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
 const logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height:props.height}}>
         {/*<img src="../../assets/images/burger-logo.png" />  this will not work since assets will not be included by webpack, so we need plugin*/}
         <img src={burgerLogo} alt="My burger" />
     </div>
--- burger-basic/src/components/UI/Modal/Modal.js
+++ burger-basic/src/components/UI/Modal/Modal.js
@@ -5,7 +5,7 @@ import Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) => (
     <Aux>
-        <Backdrop show={props.show} backdropClicked={props.modalClosed}/>
+        <Backdrop show={props.show} clicked={props.modalClosed}/>
         <div
             style={{
                 transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
