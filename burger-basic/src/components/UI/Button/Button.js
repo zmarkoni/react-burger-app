beacc968c848dd91241b9b12c0ca733447355e2b
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-08-12 21:14:46 +0200

    burger: add from validation

--- burger-basic/src/components/UI/Button/Button.js
+++ burger-basic/src/components/UI/Button/Button.js
@@ -3,6 +3,7 @@ import classes from './Button.css'
 
 const button = (props) => (
     <button
+        disabled={props.disabledProp}
         className={[classes.Button, classes[props.buttonType]].join(' ')} // to use multiple classes we need array
         onClick={props.clicked}
         >
--- burger-basic/src/containers/ContactData/ContactData.js
+++ burger-basic/src/containers/ContactData/ContactData.js
@@ -14,7 +14,12 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Your Name'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             street: {
                 elementType: 'input',
@@ -22,7 +27,12 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Street'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             zipCode: {
                 elementType: 'input',
@@ -30,7 +40,15 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'ZIP Code'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5,
+                    type: 'number'
+                },
+                valid: false,
+                touched: false
             },
             country: {
                 elementType: 'input',
@@ -38,7 +56,12 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Country'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             email: {
                 elementType: 'input',
@@ -46,7 +69,12 @@ class ContactData extends Component {
                     type: 'email',
                     placeholder: 'Your E-Mail'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -56,9 +84,12 @@ class ContactData extends Component {
                         {value: 'cheapest', displayValue: 'Cheapest'}
                     ]
                 },
-                value: ''
+                value: 'fastest',
+                validation: {},
+                valid: true
             }
         },
+        formIsValid: false,
         loading: false
     };
 
@@ -100,6 +131,33 @@ class ContactData extends Component {
 
     };
 
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        if(!rules) {
+            return true;
+        }
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
+        }
+
+        if (rules.type) {
+            isValid = typeof Number(value) === rules.type && isValid
+        }
+
+        return isValid;
+
+    }
+
     inputChangedHandler = (event, inputIndetifier) => {
         //console.log(event.target.value);
         event.preventDefault();
@@ -123,9 +181,20 @@ class ContactData extends Component {
         };
         //console.log('updatedFormElementConfig: ', updatedFormElementConfig);
         updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.touched = true;
         updatedOrderForm[inputIndetifier] = updatedFormElement;
+        console.log('updatedFormElement: ', updatedFormElement);
+
+        let formIsValid = true;
+        for(let inputIndetifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIndetifier].valid && formIsValid;
+        }
+        console.log('formIsValid: ', formIsValid);
+
         this.setState({
-            orderForm: updatedOrderForm
+            orderForm: updatedOrderForm,
+            formIsValid: formIsValid
         })
     };
 
@@ -145,9 +214,12 @@ class ContactData extends Component {
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                 ))}
-                <Button buttonType="Success">ORDER</Button>
+                <Button buttonType="Success" disabledProp={!this.state.formIsValid}>ORDER</Button>
             </form>
         );
         if (this.state.loading) {
