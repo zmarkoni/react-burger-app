9c934658a725b6afab9c1671cfe349da271c3d26
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-10-14 22:11:03 +0200

    improvment - add Lazy loading

--- burger-redux/src/App.js
+++ burger-redux/src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import asyncComponent from "./hoc/asyncComponent/asyncComponent";
+import { Route, Switch } from 'react-router-dom';
+
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from "./containers/Checkout/Checkout";
-import Orders from "./containers/Orders/Orders";
-import Auth from "./containers/Auth/Auth";
-import { Route, Switch } from 'react-router-dom';
+
+const asyncCheckout = asyncComponent(() => {
+    return import('./containers/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(() => {
+    return import('./containers/Orders/Orders');
+});
+
+const asyncAuth = asyncComponent(() => {
+    return import('./containers/Auth/Auth');
+});
 
 class App extends Component {
 
@@ -13,9 +24,9 @@ class App extends Component {
       <div>
         <Layout>
             <Switch>
-                <Route path="/checkout" component={Checkout} />
-                <Route path="/orders" component={Orders} />
-                <Route path="/auth" component={Auth} />
+                <Route path="/checkout" component={asyncCheckout} />
+                <Route path="/orders" component={asyncOrders} />
+                <Route path="/auth" component={asyncAuth} />
                 <Route path="/" exact component={BurgerBuilder} />
             </Switch>
         </Layout>
