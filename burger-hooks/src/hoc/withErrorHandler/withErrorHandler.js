7b86ba0b2f744bc28e11dfb204c11f0e5c0b7143
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-12-03 22:11:56 +0100

    Burger Hooks - refactor withErrorHandler.js

--- burger-hooks/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-hooks/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,56 +1,46 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Aux/Aux';
 
 const withErrorHandler = (WrappedComponent, axiosInstance) => {
-    return class extends Component {
+        return props => {
+            const [error, setError] = useState(null);
 
-        state = {
-            error: null
-        };
-
-        UNSAFE_componentWillMount() { // constructor can be used instead!!!
             // we want to add it first before Child components can be handled
-            this.requestInterceptor = axiosInstance.interceptors.request.use(request => {
-                this.setState({
-                    error: null
-                });
+            const requestInterceptor = axiosInstance.interceptors.request.use(request => {
+                setError(null);
                 return request;
             });
-
-            this.responseInterceptor = axiosInstance.interceptors.response.use(response => response, error => {
-                this.setState({
-                    error: error // object returned by firebase!
-                });
+            const responseInterceptor = axiosInstance.interceptors.response.use(response => response, err => {
+                setError(err);
             });
-        };
 
-        componentWillUnmount() { // if we use functional component, than we will do this in RETURN method of useEffect
-            //console.log('Will unmount from withErrorHandler', this.requestInterceptor, this.responseInterceptor);
-            axiosInstance.interceptors.request.eject(this.requestInterceptor);
-            axiosInstance.interceptors.request.eject(this.responseInterceptor);
-        };
+            useEffect(() => { // constructor can be used instead!!!
+                return function cleanup() {
+                    axiosInstance.interceptors.request.eject(requestInterceptor);
+                    axiosInstance.interceptors.request.eject(responseInterceptor);
+                };
+            }, [requestInterceptor, responseInterceptor]);
 
-        errorConfirmedHandler = () => {
-            this.setState({
-                error: null
-            });
-        };
+            const errorConfirmedHandler = () => {
+                setError(null);
+            };
 
-        render() {
             return (
                 <Aux>
                     <Modal
-                        show={this.state.error}
-                        modalClosed={this.errorConfirmedHandler}
+                        show={error}
+                        modalClosed={errorConfirmedHandler}
                     >
-                        {this.state.error ? this.state.error.message : null}
+                        {error ? error.message : null}
                     </Modal>
-                    <WrappedComponent {...this.props}/>
+                    <WrappedComponent {...props}/>
                 </Aux>
             )
+
         }
-    };
-};
+            ;
+    }
+;
 
 export default withErrorHandler;
