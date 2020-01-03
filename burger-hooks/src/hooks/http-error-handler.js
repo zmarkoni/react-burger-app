663da2ab58514ccdfc6b2332b2a5da2a96a8e0ed
Zoran Markovic <zoran.markovic@netcentric.biz>
2020-01-03 16:50:37 +0100

    burger-hooks update custom Hook

--- burger-hooks/src/hooks/http-error-handler.js
+++ burger-hooks/src/hooks/http-error-handler.js
@@ -1,27 +1,29 @@
 import {useEffect, useState} from 'react';
 
-export default httpClient => {
+// httpClient will be axios
+export default (httpClient) => {
     const [error, setError] = useState(null);
 
-    // we want to add it first before Child components can be handled
-    const requestInterceptor = httpClient.interceptors.request.use(request => {
-        setError(null);
-        return request;
-    });
-    const responseInterceptor = httpClient.interceptors.response.use(response => response, err => {
-        setError(err);
-    });
-
-    useEffect(() => { // constructor can be used instead!!!
+    useEffect(() => {
         return function cleanup() {
             httpClient.interceptors.request.eject(requestInterceptor);
             httpClient.interceptors.request.eject(responseInterceptor);
         };
     }, [requestInterceptor, responseInterceptor]);
 
-    const errorConfirmedHandler = () => {
+    // we want to add it first before Child components can be handled
+    const requestInterceptor = httpClient.interceptors.request.use(request => {
+        setError(null);
+        return request;
+    });
+
+    const responseInterceptor = httpClient.interceptors.response.use(response => response, err => {
+        setError(err);
+    });
+
+    const clearError = () => {
         setError(null);
     };
 
-    return [error, errorConfirmedHandler];
+    return [error, clearError];
 };
